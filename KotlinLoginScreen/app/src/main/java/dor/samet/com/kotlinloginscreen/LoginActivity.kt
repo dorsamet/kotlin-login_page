4fc9c8ca8ba0ed1eff19ce8c714409ed40efbc0b
dorsamet <dori.samet@gmail.com>
2018-01-31 00:00:31 +0200

    Cleanup to make code more kotlin-ish

--- KotlinLoginScreen/app/src/main/java/dor/samet/com/kotlinloginscreen/LoginActivity.kt
+++ KotlinLoginScreen/app/src/main/java/dor/samet/com/kotlinloginscreen/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import dor.samet.com.kotlinloginscreen.business_logic.use_case.UseCase
 import dor.samet.com.kotlinloginscreen.business_logic.verification.VerificationUseCase
 import dor.samet.com.kotlinloginscreen.widgets.DefaultTextWatcher
+import dor.samet.com.kotlinloginscreen.widgets.addTextWatcher
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -14,18 +15,18 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         with(edtTxtName) {
-            addTextChangedListener(object : DefaultTextWatcher() {
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    val useCase: UseCase<String, Boolean> = VerificationUseCase.VerifyUserName()
-                    useCase.executeAsync(s.toString()) {
-                        if(it) {
-                            this@with.error = null
-                        } else {
-                            this@with.error = "Wrong username"
+            addTextWatcher(
+                    onTextChanged = {
+                        s: CharSequence?, _: Int, _: Int, _: Int ->
+                        val useCase: UseCase<String, Boolean> = VerificationUseCase.VerifyUserName()
+                        useCase.executeAsync(s.toString()) {
+                            if(it) {
+                                this@with.error = null
+                            } else {
+                                this@with.error = "Wrong username"
+                            }
                         }
-                    }
-                }
-            })
+                    })
         }
     }
 }
--- KotlinLoginScreen/app/src/main/java/dor/samet/com/kotlinloginscreen/widgets/EditText.kt
+++ KotlinLoginScreen/app/src/main/java/dor/samet/com/kotlinloginscreen/widgets/EditText.kt
@@ -2,6 +2,7 @@ package dor.samet.com.kotlinloginscreen.widgets
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.EditText
 
 open class DefaultTextWatcher: TextWatcher {
     override fun afterTextChanged(s: Editable?) {}
@@ -9,4 +10,22 @@ open class DefaultTextWatcher: TextWatcher {
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+}
+
+fun EditText.addTextWatcher(beforeTextChanged: (CharSequence?, Int, Int, Int) -> Unit =
+                            { s ,start, count, after ->  },
+                            afterTextChanged: (Editable?) -> Unit = {},
+                            onTextChanged: (CharSequence?, Int, Int, Int) -> Unit =
+                            { s ,start, count, after ->  }) {
+
+    val textWatcher = object: DefaultTextWatcher() {
+        override fun afterTextChanged(s: Editable?) = afterTextChanged(s)
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) =
+                beforeTextChanged(s, start, count, after)
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) =
+                onTextChanged(s, start, before, count)
+    }
+    this.addTextChangedListener(textWatcher )
 }
